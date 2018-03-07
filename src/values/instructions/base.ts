9a405138ba1de3014264e50e7092790f4e671d7d
Fedor Indutny <fedor@indutny.com>
2018-03-07 03:57:25 -0500

    instructions: add TODO

--- src/values/instructions/base.ts
+++ src/values/instructions/base.ts
@@ -1,6 +1,7 @@
 import { Type } from '../../types';
 import { Value } from '../base';
 
+// TODO(indutny): metadata
 export abstract class Instruction extends Value {
   constructor(ty: Type, public readonly opcode: string,
               protected readonly operands: Value[]) {
