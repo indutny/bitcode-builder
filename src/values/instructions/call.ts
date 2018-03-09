762832b2b76fb826ebf643e4ed538b91b2c13a48
Fedor Indutny <fedor@indutny.com>
2018-03-09 16:08:27 -0500

    call: export `calleeSignature`

--- src/values/instructions/call.ts
+++ src/values/instructions/call.ts
@@ -2,31 +2,35 @@ import * as assert from 'assert';
 
 import * as values from '../';
 import { CallingConv } from '../../calling-conv';
+import { Signature } from '../../types';
 import { Instruction } from './base';
 
 export type CallType = 'normal' | 'tail' | 'musttail' | 'notail';
 
-function getCallType(callee: values.Value) {
+function getCalleeType(callee: values.Value): Signature {
   const ty = callee.ty;
 
   // Calling function or declaration
   if (ty.isSignature()) {
-    return ty.toSignature().returnType;
+    return ty.toSignature();
   }
 
   // A pointer to function or declaration (indirect call)
-  const pointee = ty.toPointer().to.toSignature();
-  return pointee.returnType;
+  return ty.toPointer().to.toSignature();
 }
 
 // TODO(indutny): vararg, optimizations, ret attributes
 export class Call extends Instruction {
+  public readonly calleeSignature: Signature;
+
   constructor(public readonly callee: values.Value,
               public readonly args: ReadonlyArray<values.Value>,
               public readonly callType: CallType = 'normal',
               public readonly cconv: CallingConv = 'ccc') {
-    super(getCallType(callee), 'call', [ callee ].concat(args));
+    super(getCalleeType(callee).returnType, 'call', [ callee ].concat(args));
 
+    // TODO(indutny): de-duplicate it
+    this.calleeSignature = getCalleeType(callee);
     if (!callee.isConstant()) {
       return;
     }
